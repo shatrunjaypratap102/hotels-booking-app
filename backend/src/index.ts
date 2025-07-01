afb294949189116dfc07b04028b2d600da563886
shatrunjaypratap102 <61817311+shatrunjaypratap102@users.noreply.github.com>
2025-07-01 23:19:21 +0530

    added feature to add hotel,and end2end test for feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,16 @@ import "dotenv/config";
 import mongoose from 'mongoose';
 import userRoutes from "./routes/users"
 import authRoutes from "./routes/auth"
+import useHotelRoutes from "./routes/my-hotels"
 import cookieParser from "cookie-parser";
 import path from 'path';
+import { v2 as cloudinary } from 'cloudinary';
 
+cloudinary.config({
+   cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+   api_key:process.env.CLOUDINARY_API_KEY,
+   api_secret:process.env.CLOUDINARY_API_SECRET,
+})
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 const app=express();
@@ -19,8 +26,14 @@ app.use(cors({
 }));
 
 app.use(express.static(path.join(__dirname,"../../frontend/dist")));
+
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
+app.use("/api/my-hotels",useHotelRoutes);
+
+app.get("*",(req:Request,res:Response)=>{
+    res.sendFile(path.join(__dirname,"../../frontend/dist/index.html"));
+})
 
 app.listen(3333,()=>{
     console.log("server running on localhost:3333");
